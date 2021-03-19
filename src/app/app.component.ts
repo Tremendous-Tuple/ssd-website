65da4d15b125387484021d3ab942346b57cb532d
tremendoustuple <tremendoustuple@gmail.com>
2021-03-19 16:51:00 -0400

    Added social icons and links to navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { faDiscord } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'ssd-website';
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faDiscord, faGithub);
+  }
+  discordLink = 'https://discord.gg/SXkJpdV6';
+  gitHubLink = 'https://github.com/Tremendous-Tuple';
 }
