d9247001c93325044f25f7c33ff106452a6e1867
tremendoustuple <tremendoustuple@gmail.com>
2021-03-20 03:59:02 -0400

    initial styling of home page up to recent blogs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { faDiscord } from '@fortawesome/free-brands-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
   constructor(library: FaIconLibrary) {
-    library.addIcons(faDiscord, faGithub);
+    library.addIcons(faDiscord, faFacebook, faGithub);
   }
+  title = 'ssd-website'
   discordLink = 'https://discord.gg/SXkJpdV6';
+  facebookLink = 'https://facebook.com';
   gitHubLink = 'https://github.com/Tremendous-Tuple';
 }
