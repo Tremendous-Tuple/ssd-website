0dfb553834817a8034b87b26a36c6e135dee85b2
Robert Obendorf <robertobendorf210@gmail.com>
2021-04-14 13:55:22 -0400

    upload image complete

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -58,6 +58,28 @@ export class BlogComponent implements OnInit {
     });
   }
 
+  onImageUpload(event) {
+    resizeImage({
+      file: event.target.files[0],
+      maxSize: 500
+    }).then(<Blob>(resizedImage) => {
+      console.log("upload resized image")
+
+      let file = resizedImage;
+      let reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = (e) => {
+        this.blog.img = reader.result.toString();
+        console.log("image uplaoded successfully");
+      };
+      reader.onerror = function (error) {
+        console.log('Error: ', error);
+      };
+    }).catch(function (err) {
+      console.error(err);
+  });
+  }
+
   submit() {
     if(this.newBlog){
       console.log("Creating new post...")
@@ -75,3 +97,60 @@ export class BlogComponent implements OnInit {
     }
   }
 }
+
+interface IResizeImageOptions {
+  maxSize: number;
+  file: File;
+}
+const resizeImage = (settings: IResizeImageOptions) => {
+  const file = settings.file;
+  const maxSize = settings.maxSize;
+  const reader = new FileReader();
+  const image = new Image();
+  const canvas = document.createElement('canvas');
+  const dataURItoBlob = (dataURI: string) => {
+    const bytes = dataURI.split(',')[0].indexOf('base64') >= 0 ?
+        atob(dataURI.split(',')[1]) :
+        unescape(dataURI.split(',')[1]);
+    const mime = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    const max = bytes.length;
+    const ia = new Uint8Array(max);
+    for (var i = 0; i < max; i++) ia[i] = bytes.charCodeAt(i);
+    return new Blob([ia], {type:mime});
+  };
+  const resize = () => {
+    let width = image.width;
+    let height = image.height;
+
+    if (width > height) {
+        if (width > maxSize) {
+            height *= maxSize / width;
+            width = maxSize;
+        }
+    } else {
+        if (height > maxSize) {
+            width *= maxSize / height;
+            height = maxSize;
+        }
+    }
+
+    canvas.width = width;
+    canvas.height = height;
+    canvas.getContext('2d').drawImage(image, 0, 0, width, height);
+    let dataUrl = canvas.toDataURL('image/jpeg');
+    return dataURItoBlob(dataUrl);
+  };
+
+  return new Promise((ok, no) => {
+      if (!file.type.match(/image.*/)) {
+        no(new Error("Not an image"));
+        return;
+      }
+
+      reader.onload = (readerEvent: any) => {
+        image.onload = () => ok(resize());
+        image.src = readerEvent.target.result;
+      };
+      reader.readAsDataURL(file);
+  })    
+};
