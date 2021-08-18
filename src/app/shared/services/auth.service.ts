b20bba0ebab2b650ec8ed963404379a1f8127b03
EricNavar <ericnavar@ufl.edu>
2021-08-18 13:47:45 -0400

    add eslint rules change description in HTML meta tag add manifest.json add npm script for an optimized build

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, NgZone } from '@angular/core';
 import { User } from "../services/user";
-import { auth } from 'firebase/app';
 import { AngularFireAuth } from "@angular/fire/auth";
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 //import { AngularFirestore,AngularFirestoreDocument  } from '@angular/fire/firestore';
